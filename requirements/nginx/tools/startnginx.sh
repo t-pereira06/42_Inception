e5f02ee23e9cf6ca1ce2c00f85c557b0f4db7c1a
Tiago Pereira <118270669+t-pereira06@users.noreply.github.com>
2024-01-24 07:22:37 -0500

    Delete requirements directory

--- requirements/nginx/tools/startnginx.sh
+++
@@ -1,9 +0,0 @@
-#This steps need to be done because nginx can't use the env variables
-# So here, we change the variables to the value of env variables on a provided .env
-# or add them to env
-
-sed -i "s/__SCHOOL_USERNAME__/$SCHOOL_USERNAME/g" /etc/nginx/nginx.conf
-sed -i "s/__NGINX_WORDPRESS_CONTAINER_NAME__/$NGINX_WORDPRESS_CONTAINER_NAME/g" /etc/nginx/nginx.conf
-sed -i "s/__NGINX_WORDPRESS_PORT__/$NGINX_WORDPRESS_PORT/g" /etc/nginx/nginx.conf
-
-nginx -g "daemon off;"
