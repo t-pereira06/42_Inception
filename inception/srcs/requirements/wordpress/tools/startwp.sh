d2e652370a19f06a6a7829c0363fc5d85c078aae
Tiago Pereira <118270669+t-pereira06@users.noreply.github.com>
2024-01-29 04:50:27 -0500

    Add files via upload

--- inception/srcs/requirements/wordpress/tools/startwp.sh
+++ inception/srcs/requirements/wordpress/tools/startwp.sh
@@ -1,5 +1,5 @@
-# Starting php8.2-fpm creates /run/php/php7.1-fpm.sock
-# Now the php-fpm8.2 -F -R works, before doing service php8.2-fpm it wouldn't work
-service php8.2-fpm start
-service php8.2-fpm stop
-php-fpm8.2 -F -R
+# Starting php7.4-fpm creates /run/php/php7.1-fpm.sock
+# Now the php-fpm7.4 -F -R works, before doing service php7.4-fpm it wouldn't work
+service php7.4-fpm start
+service php7.4-fpm stop
+php-fpm7.4 -F -R
